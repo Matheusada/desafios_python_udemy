7b3f792e4cfec53b211ca670f434dc0a4e736ff9
Matheus A. <Matheus.a.d.a@hotmail.com>
2025-08-08 15:31:52 -0300

    todas as formas de imprimir

--- desafio2.py
+++ desafio2.py
@@ -2,3 +2,6 @@ first_name = 'Matheus'
 age = 32
 
 print(f'Olá, meu nome é {first_name} e eu tenho {age} anos')
+print('Olá, meu nome é {} e eu tenho {} anos'.format(first_name,age))
+print('Olá, meu nome é %s e eu tenho %d anos'%(first_name, age))
+print('Olá, meu nome é' + str(first_name) + 'e eu tenho' + str(age) + 'anos')
