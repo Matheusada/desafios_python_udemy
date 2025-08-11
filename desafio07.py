cdf7874f5ccbb16b82782caffb9dab4c15ba95fa
Matheus A. <Matheus.a.d.a@hotmail.com>
2025-08-11 15:39:07 -0300

    atualização para forma mais limpa

--- desafio07.py
+++ desafio07.py
@@ -1,3 +1,7 @@
-frutas = ['maça', 'banana', 'manga', 'uva']
+frutas = [1,2,'maça', 'banana', 'manga', 'uva']
 
-print(frutas[0], frutas[len(frutas)-1])
+#print(frutas[0], frutas[len(frutas)-1]) - 1ª SOLUÇÃO
+#RESOLUÇÃO MAIS LIMPA/CORRETA - SE EU PEGAR O [-1] ele sempre vai pegar 
+#O último item ! 
+
+print('O primeiro item da lista é:', frutas[0],'. E, o último item da lista, é:', frutas[-1])
