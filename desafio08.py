07447da85c31fe20046e889d4aed976ceb9b4b66
Matheus A. <Matheus.a.d.a@hotmail.com>
2025-08-11 17:51:47 -0300

    Métodos_desafio08

--- desafio08.py
+++ desafio08.py
@@ -13,3 +13,10 @@ frutas[1:3] = ['abacaxi','abacate']
 print(frutas)
 #PARTICULARIDADES => frutas[1:3] de 1 até 3, mas sem 
 # incluir o 3
+#no caso abaixo, há a substituição da banana por 'abacaxi' e 'abacate'
+frutas[1:2] = ['abacaxi','abacate']
+print(frutas)
+#ADICIONADO UM OBJETO EM UM ÍNDICE ESPECÍFICO
+#INSERT -> frutas.insert(index,object)
+frutas.insert(2,'Maracujá')
+print(frutas)
