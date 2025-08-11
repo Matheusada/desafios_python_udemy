67bfd3f77f27f44759c5bf5433ed6745b1f69ec6
Matheus A. <Matheus.a.d.a@hotmail.com>
2025-08-11 17:26:11 -0300

    desafio08 atualizações

--- desafio08.py
+++ desafio08.py
@@ -6,3 +6,10 @@ print(frutas)
 #MÉTODOS (EXTENSÕES)
 frutas.append('abacaxi')
 print(frutas)
+
+#CONTEÚDOS
+#alterando 2 itens ao mesmo tempo
+frutas[1:3] = ['abacaxi','abacate']
+print(frutas)
+#PARTICULARIDADES => frutas[1:3] de 1 até 3, mas sem 
+# incluir o 3
