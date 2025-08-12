f1b9560594874ee6a9b5c2e541a20ec8ea9a4a6b
Matheus A. <Matheus.a.d.a@hotmail.com>
2025-08-12 15:03:46 -0300

    resolução desafio 12

---
+++ desafio12.py
@@ -0,0 +1,6 @@
+frutas = ['banana', 'uva', 'maça']
+vegetais = ['couve', 'alface', 'cebolinha']
+
+for i in range(0,3):
+        for j in range(0,3):
+                print(frutas[i],'--',vegetais[j]) 
