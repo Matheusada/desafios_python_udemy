4d3745b9d56346593eeee65abdc8b61ebc5761f4
Matheus A. <Matheus.a.d.a@hotmail.com>
2025-08-12 16:47:28 -0300

    desafio14

---
+++ desafio14.py
@@ -0,0 +1,19 @@
+print('Contagem com o break')
+n = 1 
+while n <=10:
+    print(n)
+    n +=1
+    if n == 6:
+        break
+
+print('Contagem com o continue')
+j =1
+while j <= 10:
+    print(j)
+    j += 1
+    if j ==5:
+        j += 1
+    else:
+        continue
+        
+        
